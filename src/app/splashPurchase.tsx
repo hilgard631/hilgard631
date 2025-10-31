e2249acef0bfcc996d036b00817fe8af2dddabd4
Cal <cydoval@mac.com>
2025-10-31 10:22:54 -0700

    new info

--- src/app/splashPurchase.tsx
+++ src/app/splashPurchase.tsx
@@ -21,7 +21,7 @@ const SplashPurchase: React.FC = () => {
             <h2 className="text-xl font-bold mb-2">November</h2>
             <ul className="list-inside space-y-1">
               <li>Sunday, November 2, 11am – 1pm</li>
-              <li>Thursday, November 13, 2pm – 4pm</li>
+              <li>Thursday, November 13, 11am – 1pm</li>
               <li>Tuesday, November 25, 11am – 1pm</li>
             </ul>
           </div>
