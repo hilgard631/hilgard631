271dd9d1752a264223dee53c3bda063e93ccea93
Cal <cydoval@mac.com>
2026-01-22 16:53:59 -0800

    sold out

--- src/app/components/WineGrid.tsx
+++ src/app/components/WineGrid.tsx
@@ -88,7 +88,7 @@ const wines = [
   {
     name: "Meleagris",
     type: "Petite Sirah",
-    inventory: "low stock",
+    inventory: "SOLD OUT",
     year: 2024,
     price: 40.0,
     location: "Yolo County",
