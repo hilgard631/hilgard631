e2a435964b6fb42b055f38337767c71e40e6f54d
Cal <cydoval@mac.com>
2026-07-13 09:24:43 -0700

    dates

--- src/app/splashPurchase.tsx
+++ src/app/splashPurchase.tsx
@@ -35,16 +35,8 @@ const SplashPurchase: React.FC = () => {
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 pb-4">
           <div>
-            <h2 className="text-lg font-bold mb-1">June</h2>
-            <ul className="list-inside space-y-1">
-              <li>Tuesday, June 16, 12-4 PM</li>
-              <li>Friday, June 26, 12-2 PM</li>
-            </ul>
-          </div>
-          <div>
             <h2 className="text-lg font-bold mb-1">July</h2>
             <ul className="list-inside space-y-1">
-              <li>Friday, July 10, 12-2 PM</li>
               <li>Friday, July 24, 2-4 PM</li>
             </ul>
           </div>
